be2821de7c3fde2d7025785e628b506699e81479
amolkapadi <amolakapadi@gmail.com>
2023-06-20 14:57:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Routes,Route,BrowserRouter} from "react-router-dom"
+import Dashboard from './Page/Dashboard'
+import Product from './Page/Product'
+import AddProduct from './Page/AddProduct'
+import Settings from './Page/Settings'
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ <>
+  <BrowserRouter>
+    <Routes>
+        <Route path='/' element={<Dashboard />}></Route>
+        <Route path='/product' element={<Product />}></Route>
+        <Route path='/addproduct' element={<AddProduct />}></Route>
+        <Route path='/settings' element={<Settings />}></Route>
+    </Routes>
+  </BrowserRouter>
+ </>
+  )
 }
-
-export default App;
